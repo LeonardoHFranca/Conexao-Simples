eab43143312995d66e45df5ed191838c1a71ba9c
leonardofranca <leonardofranca1210@gmail.com>
2025-04-11 15:18:52 -0300

    second

--- crud/src/main/java/com/leonardo/crud/controllers/ProductController.java
+++ crud/src/main/java/com/leonardo/crud/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.leonardo.crud.controllers;
 
+import com.leonardo.crud.dtos.ProductDto;
 import com.leonardo.crud.model.Product;
 import com.leonardo.crud.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -23,4 +24,42 @@ public class ProductController {
         List<Product> listproducts = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listproducts);
     }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity getById(@PathVariable(value = "id") Integer id){
+        Optional product = repository.findById(id);
+        if(product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(product.get());
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ProductDto dto){
+        var product = new Product();
+        BeanUtils.copyProperties(dto, product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity delete(@PathVariable(value = "id") Integer id){
+        Optional<Product> product = repository.findById(id);
+        if(product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        repository.delete(product.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted");
+    }
+    @PutMapping("/{id}")
+    public  ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody ProductDto dto){
+        Optional<Product> product = repository.findById(id);
+        if(product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        var productModel = product.get();
+        BeanUtils.copyProperties(dto, productModel);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(productModel));
+    }
+
 }
+
---
+++ crud/src/main/java/com/leonardo/crud/dtos/ProductDto.java
@@ -0,0 +1,4 @@
+package com.leonardo.crud.dtos;
+
+public record ProductDto(String name,Long price) {
+}
